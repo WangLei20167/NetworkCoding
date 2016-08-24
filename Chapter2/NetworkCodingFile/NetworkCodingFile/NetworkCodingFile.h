230655600b0d321357b1c5a689e65a7badf66739
WangLei20167 <dizairenwei@163.com>
2016-08-24 14:24:00 +0800

    update NetworkCodingFile

---
+++ Chapter2/NetworkCodingFile/NetworkCodingFile/NetworkCodingFile.h
@@ -0,0 +1,32 @@
+
+// NetworkCodingFile.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CNetworkCodingFileApp: 
+// 有关此类的实现，请参阅 NetworkCodingFile.cpp
+//
+
+class CNetworkCodingFileApp : public CWinApp
+{
+public:
+	CNetworkCodingFileApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CNetworkCodingFileApp theApp;
