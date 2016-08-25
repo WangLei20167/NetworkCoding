bbead63cbf6f41d2895e07d0a459b4f7c3998ecc
WangLei20167 <dizairenwei@163.com>
2016-08-25 21:16:33 +0800

    change gf.c

---
+++ Chapter2/Calculator/Calculator/CalculatorDlg.h
@@ -0,0 +1,49 @@
+
+// CalculatorDlg.h : header file
+//
+
+#pragma once
+
+
+// CCalculatorDlg dialog
+class CCalculatorDlg : public CDialogEx
+{
+// Construction
+public:
+	CCalculatorDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_CALCULATOR_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	int m_nAlter1;
+	int m_nAlter2;
+	int m_nResult;
+	CString m_chOperator;
+	unsigned int m_nType;
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedButton3();
+	afx_msg void OnBnClickedButton4();
+	afx_msg void OnBnClickedButton5();
+	afx_msg void OnBnClickedButton6();
+	int m;
+	afx_msg void OnEnChangeEdit4();
+	afx_msg void OnStnClickedmchoperator();
+	afx_msg void OnEnChangeEdit2();
+	afx_msg void OnEnChangeEdit1();
+};
